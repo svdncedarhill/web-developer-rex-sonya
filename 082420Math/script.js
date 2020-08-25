d027d7b6e09fd6d18376dd03cce07bf7d9292d03
Sonya Daniel <itclassroomschool>
2020-08-24 19:23:31 -0500

    082420

---
+++ 082420Math/script.js
@@ -0,0 +1,24 @@
+console.log(`PI Value = ${Math.PI}`);
+console.log('E = ' + Math.E);
+
+console.log(`abs(-15) = ${Math.abs(-15)}`);
+console.log(`max(10,4,23,7) = ${Math.max(10,4,23,7)}`);
+console.log(`min(10,4,23,7) = ${Math.min(10,4,23,7)}`);
+console.log(`sqrt(25) = ${Math.sqrt(25)}`);
+console.log(`power(3,3) = ${Math.pow(3,3)}`);
+
+console.log(`floor(10.9) = ${Math.floor(10.9)}`);
+console.log(`ceil(10.9) = ${Math.ceil(10.9)}`);
+
+console.log(`sin(0) = ${Math.sin(0)}`);
+console.log(`cos(0) = ${Math.cos(0)}`);
+
+console.log(`round(10.4) = ${Math.round(10.4)}`);
+console.log(`round(10.5) = ${Math.round(10.5))}`);
+
+let rand = Math.random();
+console.log(`random number = ${rand}`);
+rand=rand*10000;
+console.log(rand);
+rand=Math.floor(rand);
+console.log(`OTP = ${rand}`)
