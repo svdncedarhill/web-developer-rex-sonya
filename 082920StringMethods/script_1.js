530c0fe7739c6c6c8df11686ee0ed66edb79dc53
Sonya Daniel <itclassroomschool>
2020-09-02 18:31:07 -0500

    090220

---
+++ 082920StringMethods/script_1.js
@@ -0,0 +1,22 @@
+let str = 'Java Script is a Scripting Language';
+console.log(str);
+
+let portion_1 = str.substr(5); //5-starting index
+console.log(portion_1);
+
+let portion_2 = str.substr(5,6); //5-string inded, 6 no of characters from starting index
+conosle.log(portion_2); //Script
+
+let portion_3 = str.substring(5,8); //5-start index, 8-end index
+console.log(portion_3); //Scr
+
+let portion_4 = str.substring(8,5);
+console.log(portion_5) //Scr
+
+let portion_5 = str.slice(5,8);
+console.log(portion_5) //Scr
+
+let portion_6 = str.slice(8,5);
+console.log(portion_6) //empty string
+
+let email
